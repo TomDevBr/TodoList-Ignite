86fce98323547ede076b0a2caace1aa0c32100f3
TomDevBr <tomasjocalheiros@gmail.com>
2022-12-31 10:48:44 -0300

    finish style for task component

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -2,12 +2,16 @@ import style from './Task.module.scss'
 
 export default function Task(){
     return (
+        <>
         <div className={style.taskContainer}>
-            <label htmlFor="checkbox" >
-            <input type="checkbox"  className={style.checkbox} name="checkbox" id="checkbox" checked/>
-            </label>
-            <p className={style.nameTask}> Lorem ipsum dolor sit amet consectetur adipisicing eli</p>
+            <div className={style.customCheckbox}>
+                <input type="checkbox" id="checkbox" />
+                <label htmlFor="checkbox" className={style.checkboxLabel}>
+                <p> Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+                </label>
+            </div>
             <img src="src/assets/delete.svg" alt="delete" />
         </div>
+        </>
     )
 }
