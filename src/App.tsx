ccba214296d62cf5c7a0d463353395666e35b5d0
TomDevBr <tomasjocalheiros@gmail.com>
2022-12-31 19:07:57 -0300

    feat: create a object for new tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,95 @@
 import Header from "./components/Header/Header";
+import { v4 } from "uuid";
 import style from "./App.module.scss";
 import Task from "./components/Task/Task";
+import React, { ChangeEventHandler, FormEvent, FormEventHandler, SetStateAction, useState } from "react";
+
+
+const tasks = [
+  {
+    id: v4(),
+    title: ["primeira task"],
+    isCompleted: true,
+  },
+  {
+    id: v4(),
+    title: ["segunda task"],
+    isCompleted: false,
+  },
+  {
+    id: v4(),
+    title: ["terceira task"],
+    isCompleted: true,
+  },
+]
+
 
 function App() {
+  const [ tasksCompleteds, setTasksCompleteds] = useState(0)
+  const [ onCheck, setOnCheck] = useState(false)
+  const [ titleTask, setTitleTask] = useState(['escreva sua primeira task'])
+  const [ taskCreated, setTaskCreated ] = useState("")
+
+  function createNewTask ( event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    setTitleTask([...titleTask, taskCreated])
+
+    const newTask = {
+      id: v4(),
+      title: titleTask,
+      isCompleted: onCheck,
+    }
+  
+    tasks.push(newTask)
+
+  }
+
+
+
+
+  function handleNewTaskText ( event: React.ChangeEvent<HTMLInputElement>) {
+    setTaskCreated(event.target.value)
+
+   
+  }
+
+ 
+
+  function taskChecked() {
+    setOnCheck(true)
+  }
+  console.log(tasks)
+
+
   return (
     <>
       <div className={style.appContainer}>
         <Header />
         <div className={style.appGrid}>
-        <div className={style.inputContainer}>
+          <form onSubmit={createNewTask} className={style.inputContainer}>
           <label htmlFor="input">
-            <input type="text" name="input" placeholder="adicione uma nova tarefa" />
+            <input type="text" onChange={handleNewTaskText} name="input"  placeholder="adicione uma nova tarefa" />
           </label>
-          <button>Criar <img src="src/assets/plus.svg" alt="plus" /></button>
-        </div>
+          <button type="submit">Criar <img src="src/assets/plus.svg" alt="plus" /></button>
+          </form>
         <section className={style.listContainer}>
           <div className={style.listHeader}>
             <p className={style.tasksCreateds}>Taterafas criadas <div className={style.count}>0</div></p> 
-            <p className={style.tasksCompleteds}>Concluidas <div className={style.count}>0</div></p>    
+            <p className={style.tasksCompleteds}>Concluidas <div className={style.count}>{tasksCompleteds}</div></p>    
           </div>
           <div className={style.listTasks}>
               {/* <img src="src/assets/Clipboard.svg" alt="clipboard" />
               <h2 className={style.firstLineText}>Você ainda não tem tarefas cadastradas</h2>
               <h2>Crie tarefas e organize seus itens a fazer</h2> */}
-              { <Task/>}
 
-
+              {
+                tasks.map( task => { 
+                  return(
+                    <Task  title={task.title} id={task.id} isChecked={task.isCompleted} onCheckTask={taskChecked}   />
+                  )
+                })
+              }
+            
           </div>
         </section>
         </div>
--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,17 +1,29 @@
-import style from './Task.module.scss'
+import { useState } from "react";
+import { v4  } from 'uuid'
+import style from "./Task.module.scss";
+interface ITasks {
+  title: string[];
+  id: string;
+  isChecked: boolean;
+  onCheckTask(): void;
+}
 
-export default function Task(){
-    return (
-        <>
-        <div className={style.taskContainer}>
-            <div className={style.customCheckbox}>
-                <input type="checkbox" id="checkbox" />
-                <label htmlFor="checkbox" className={style.checkboxLabel}>
-                <p> Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
-                </label>
-            </div>
-            <img src="src/assets/delete.svg" alt="delete" />
+export default function Task({ title, id, isChecked, onCheckTask }: ITasks) {
+
+    function checkTask() {
+        onCheckTask();
+    }
+  return (
+    <>
+      <div className={style.taskContainer}>
+        <div className={style.customCheckbox}>
+            <input type="checkbox" onClick={checkTask} id={id} data-checked={isChecked}/>
+          <label htmlFor={id}  className={style.checkboxLabel}>
+            <p>{title}</p>
+          </label>
         </div>
-        </>
-    )
+        <img src="src/assets/delete.svg" alt="delete" />
+      </div>
+    </>
+  );
 }
